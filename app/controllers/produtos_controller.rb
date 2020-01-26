7fa82927ce74c627d3ced2e24b25c19e1e742b76
Joao Almeida <wagner.dev1337@gmail.com>
2020-01-26 03:17:43 -0300

    criando form e adicionando botstrap no form tbm

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,7 +1,13 @@
 class ProdutosController < ApplicationController
 
     def index
-        @produtos = Produto.order(name: :desc).limit 2
+        @produtos = Produto.order(name: :desc).limit 5
         @produto_com_desconto = Produto.order(:preco).limit 1
+    end
+
+    def create
+        produto = params.require(:produto).permit(:nome, :descricao, :preco, :quantidade)
+        Produto.create produto
+        redirect_to root_path
     end
 end
