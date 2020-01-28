ceee7c8e947ec91050f5f99e1513f2bcae21229a
Joao Almeida <wagner.dev1337@gmail.com>
2020-01-27 21:46:01 -0300

    deletando produtos

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,13 +1,19 @@
 class ProdutosController < ApplicationController
 
     def index
-        @produtos = Produto.order(name: :desc).limit 5
+        @produtos = Produto.order(name: :desc).limit 6
         @produto_com_desconto = Produto.order(:preco).limit 1
     end
 
     def create
         produto = params.require(:produto).permit(:nome, :descricao, :preco, :quantidade)
         Produto.create produto
+        redirect_to root_path
+    end
+
+    def destroy
+        id = params[:id]
+        Produto.destroy id
         redirect_to root_path
     end
 end
