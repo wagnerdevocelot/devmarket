a273bbbd9c71f310aa0169e8d9ff51021124da74
Joao Almeida <wagner.dev1337@gmail.com>
2020-01-28 11:40:07 -0300

    criando departamentos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_26_035517) do
+ActiveRecord::Schema.define(version: 2020_01_28_141121) do
+
+  create_table "departamentos", force: :cascade do |t|
+    t.string "nome"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "produtos", force: :cascade do |t|
     t.string "nome"
